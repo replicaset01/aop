e248817da8c0f25f9f89f84f53a5f02d53e16dcc
roal <replicaset01@github.com>
2022-10-18 16:09:31 +0900

    Initial commit

---
+++ src/test/java/start/aop/AopTest.java
@@ -0,0 +1,44 @@
+package start.aop;
+
+// test/java/start.aop/AopTest.java
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import start.aop.order.OrderRepository;
+import start.aop.order.OrderService;
+import start.aop.order.aop.Aspect1;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@Slf4j
+@SpringBootTest
+@Import(Aspect1.class)
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
